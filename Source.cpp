58d3b811d6b5013be9ef2833d9051b2a020e0a62
booglebeast <31456986+booglebeast@users.noreply.github.com>
2018-01-17 08:36:53 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main() {
 
-	cout << "yo" << endl;
+	cout << "Ready for a joke?" << endl;
+	system("pause");
+	cout << "The oldest computer can be traced back to Adam and Eve."<<endl;
+	system("pause");
+	cout << "It was an apple but with extremely limited memory. Just 1 byte. And then everything crashed." << endl;
+	system("pause");
+	
 }
